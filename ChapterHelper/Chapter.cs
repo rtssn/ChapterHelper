7bf15c9c7d327fb8b6c2d693e65427139776beb3
motoki <krkrmfmf@outlook.jp>
2022-05-28 18:58:14 +0900

    プロジェクト ファイルを追加します。

---
+++ ChapterHelper/Chapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterHelper
+{
+    internal class Chapter
+    {
+        public string ChapterTitle
+        {
+            get;
+            set;
+        }
+
+        public int Timestamp
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ChapterHelper/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ChapterHelper
+{
+    class Program
+    {
+        private static Regex TimeLineRegex = new Regex(@"(\d+):(\d+):(\d+)\s(.*)");
+
+        static void Main(string[] args)
+        {
+            if(args.Count() != 2)
+            {
+                Console.WriteLine("引数に間違いがあります。");
+
+                return;
+            }
+
+            string inputFile = args[0];
+            string outputFile = args[1];
+
+            bool isInputfileExsits = File.Exists(inputFile);
+
+            if(isInputfileExsits == false)
+            {
+                Console.WriteLine("入力ファイルが存在しません。");
+
+                return;
+            }
+
+            List<Chapter> chapters = new List<Chapter>();
+
+            using(StreamReader reader = new StreamReader(inputFile))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string? line = reader.ReadLine();
+
+                    if (line != null)
+                    {
+                        Match match = TimeLineRegex.Match(line);
+
+                        int hour = 0;
+                        int min = 0;
+                        int second = 0;
+                        string chapterTitle = match.Groups[4].Value;
+
+                        int.TryParse(match.Groups[1].Value, out hour);
+                        int.TryParse(match.Groups[2].Value, out min);
+                        int.TryParse(match.Groups[3].Value, out second);
+
+                        int timestamp = (hour * 60 + min * 60 + second) * 1000;
+
+                        Chapter chapter = new Chapter();
+                        chapter.ChapterTitle = chapterTitle;
+                        chapter.Timestamp = timestamp;
+
+                        chapters.Add(chapter);
+                    }
+                }
+            }
+        }
+    }
+}
